66a5e16c213e872f60b97d3ae27af9d51fdcb390
carlosjc31 <carlosjc31@hotmail.com>
2024-09-17 11:21:09 -0300

    Product-form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { ProductsTableComponent } from './products-table/products-table.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductFormComponent } from './product-form/product-form.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: 'products', component: ProductsTableComponent},
+  {path: 'product/:id', component: ProductFormComponent}
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
